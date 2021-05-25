c13db18c66c3c60ec95d35805c851c046cb5279b
Corentin Bessire <corentin.bessire@devfactory.ch>
2021-05-25 10:13:26 +0200

    Books #0 - Basic Theming of Form

--- web/modules/books_book_managment/src/Form/AddBookForm.php
+++ web/modules/books_book_managment/src/Form/AddBookForm.php
@@ -22,18 +22,47 @@ class AddBookForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $form['isbn'] = [
+    $form['#attributes']['class'][] = 'w-full';
+    $form['#attributes']['class'][] = 'max-w-lg';
+
+    $form['wrapper'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [
+          'flex',
+          'flex-wrap',
+          '-mx-3',
+          'mb-6',
+        ],
+      ],
+    ];
+
+    $form['wrapper']['isbn'] = [
       '#type' => 'textfield',
       '#title' => $this->t('ISBN'),
       '#required' => TRUE,
     ];
 
-    $form['actions'] = [
+    $form['wrapper']['actions'] = [
       '#type' => 'actions',
     ];
-    $form['actions']['submit'] = [
+    $form['wrapper']['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Add'),
+      '#attributes' => [
+        'class' => [
+          'shadow',
+          'bg-purple-500',
+          'hover:bg-purple-400',
+          'focus:shadow-outline',
+          'focus:outline-none',
+          'text-white',
+          'font-bold',
+          'py-2',
+          'px-4',
+          'rounded',
+        ],
+      ],
     ];
 
     return $form;
