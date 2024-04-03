c5049c958b927074ea57c0c7c68630ba29a495b0
Corentin Bessire <corentin.bessire@liip.ch>
2024-04-03 08:50:24 +0200

    Update Theme with Yarn Build

--- web/themes/custom/books/js/menu.js
+++ web/themes/custom/books/js/menu.js
@@ -1,7 +1,7 @@
-let toggleBtn = document.querySelector("#navbar-toggle");
-let collapse = document.querySelector("#navbar-collapse");
+const toggleBtn = document.querySelector('#navbar-toggle');
+const collapse = document.querySelector('#navbar-collapse');
 
 toggleBtn.onclick = () => {
-  collapse.classList.toggle("hidden");
-  collapse.classList.toggle("flex");
+  collapse.classList.toggle('hidden');
+  collapse.classList.toggle('flex');
 };
--- web/themes/custom/books/tailwind.config.js
+++ web/themes/custom/books/tailwind.config.js
@@ -1,19 +1,4 @@
 module.exports = {
-  purge: {
-    content: [
-      './templates/**/*.html.twig',
-      './js/**/*.js',
-    ],
-    options: {
-      keyframes: true,
-    },
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+  mode: 'jit',
+  content: ['./templates/**/*.html.twig', './books.theme', './js/**/*.js'],
+};
