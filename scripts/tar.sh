dbe02c96b8a5f04453c01d19bd406d1553894460
Corentin Bessire <corentin.bessire@liip.ch>
2024-11-26 11:33:36 +0100

    Update Drupal and Contrib modules

--- scripts/tar.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-tar --exclude='./web/sites' --exclude='./vendor' --exclude='./web/core' --exclude='./web/modules/contrib' --exclude='./web/themes/contrib' --exclude='./web/profiles/contrib' --exclude='./web/libraries' -zcvf $(date "+%Y%m%d_%H%M").tgz .
